2b7eac2bec7b64f628ebd64e2f0a70a14b89f582
Koushik Das <koushik78bd@gmail.com>
2024-08-11 07:06:56 +0600

    jwt file working

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,27 +1,26 @@
-require("dotenv").config();
-import { Response } from "express";
-import { IUser } from "../models/user.model";
+require('dotenv').config()
+import { Response } from 'express'
+import { IUser } from '../models/user.model'
 // import { redis } from "./redis";
 
 interface ITokenOptions {
-    expires: Date;
-    maxAge: number;
-    httpOnly: boolean;
-    sameSite: 'lax' | 'strict' | 'none' | undefined;
-    secure?: boolean;
+  expires: Date
+  maxAge: number
+  httpOnly: boolean
+  sameSite: 'lax' | 'strict' | 'none' | undefined
+  secure?: boolean
 }
 
-export const sendToken = (user: IUser, statusCode: number, res:Response) => {
+export const sendToken = (user: IUser, statusCode: number, res: Response) => {
+  const accessToken = user.SignAccessToken()
+  const refreshToken = user.SignRefreshToken()
 
-    const accessToken = user.SignAccessToken();
-    const refreshToken = user.SignRefreshToken();
+  // upload session to redis
 
-    // upload session to redis
+  //  parse environment variable to integrates with fallback values
 
-
-    //  parse environment variable to integrates with fallback values
-
-    // const accessTokenExpires = parseInt(process.removeListener.ACCESS_TOKEN_EXPIRE || '300', 10)
-
-    
+  const accessTokenExpires = parseInt(
+    process.removeListener.ACCESS_TOKEN_EXPIRE || '300',
+    10
+  )
 }
